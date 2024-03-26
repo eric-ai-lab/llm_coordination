2e0018ef0de971c597e3003e2f4a8ec715e4a6dc
noStackEngineer <anthonyreynax@gmail.com>
2024-03-25 17:17:37 -0700

    collab_escape_benchmarking: multiple playthroughs, save metrics

--- src/agentic_evals/collab_escape_benchmarking.py
+++ src/agentic_evals/collab_escape_benchmarking.py
@@ -1,9 +1,36 @@
 import sys 
+import numpy as np
+
 sys.path.append('..')
 from collab_escape.CollabEscapeMDP import Game
 from llm_coordination_agents.collab_escape_agent import LLMAgent
 
 
 if __name__ == "__main__":
-    game = Game()
-    game.play()
+    results = []
+    turn_counts = []
+    NUM_TRIALS = 3
+    for i in range(NUM_TRIALS):
+        # init game
+        game = Game()
+
+        # run game, start to finish
+        outcome, turns = game.play()
+
+        results.append(outcome)
+        turn_counts.append(turns)
+    
+
+    wins = results.count('win')
+    escape_rate = (wins / NUM_TRIALS) * 100
+   
+    results_to_save = f'''
+    Escape Rate: {escape_rate}%
+    Average Turns to Escape: {np.mean(turn_counts)}
+    Standard Error: {np.std(turn_counts) / np.sqrt(NUM_TRIALS)}
+    Turn Counts: {turn_counts}
+    '''
+
+    # Save to a text file
+    with open('collab_escape_results.txt', 'w') as file:
+        file.write(results_to_save)
